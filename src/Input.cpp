4452f104b1de5c49ad92685f8a9891258cc1dc67
Nick Cardullo <nick.cardullo@gmail.com>
2011-08-04 13:41:52 -0400

    Added the beginnings of promptUser. The method will provide a consistent prompt for any user input.

--- src/Input.cpp
+++ src/Input.cpp
@@ -4,5 +4,14 @@
 #include <iostream>
 #include <string>
 #include "Game.h"
+#include "Input.h"
 
 using namespace std;
+
+string Input::promptUser(string promptText)
+{
+	string i;	// Temporary storage for user input
+	cout << ">> " << promptText << endl;	// Print a nice prompt
+	cin >> i;	// Recieve and return the user input
+	return i;	// TODO: Some sort of bounds-checking?
+}
--- src/Input.h
+++ src/Input.h
@@ -1,6 +1,11 @@
 // Input class header
 // Provides functionality for recieving input
 
+#include <iostream>
+#include <string>
+using namespace std;
+
 class Input {
-	
+	public:
+		string promptUser(string promptText);
 };
