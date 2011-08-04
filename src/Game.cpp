6dd49c3a00c7caa9c2aa791dd6a32e95688dacc5
Nick Cardullo <nick.cardullo@gmail.com>
2011-08-04 13:43:09 -0400

    Some more skeleton code to test the Input class0

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,10 +1,21 @@
 // Main Game class
 // Also provides main()
 
+#include <string>
 #include "Game.h"
 #include "Input.h"
 #include "Output.h"
 
 int main() {
+	// Setup our Input/Output objects here
+	Input in;
+	Output out;
+	
+	// Begin menu loop. Should be in own function
+	// We also need to print the actual menu
+	string choice;
+	choice = in.promptUser("What is your selection?");
+	
+	// If we made it this far, let's call it a success!
 	return 0;
 }
